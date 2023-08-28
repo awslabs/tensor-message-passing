fe32ea099eac46fb9443c2e300395b65943362b0
Nicola Pancotti <npancott@amazon.com>
2023-08-27 18:09:14 -0700

    adde MIS

--- tnmpa/models/mis.py
+++ tnmpa/models/mis.py
@@ -8,29 +8,31 @@ from tnmpa.models import Factor, FactorGraph, Variable
 
 
 class MIS(FactorGraph):
-    def __init__(self, N, M, seed=None):
-        graph = nx.gnm_random_graph(N, M, seed)
-        nx.relabel_nodes(graph, {i: f"v{i}" for i in range(N)}, copy=False)
-        factors = [
-            Factor(f"c{k}", [Variable(e[0]), Variable(e[1])])
-            for k, e in enumerate(graph.edges)
-        ]
-        super().__init__(factors)
+    def __init__(self, N, M, mu, seed=None):
+        self.mu = mu
+        super().__init__([])
+        self.graph = nx.gnm_random_graph(N, M, seed)
+        remap = {n: f"v{n}" for n in self.graph.nodes}
+        nx.relabel_nodes(self.graph, remap, copy=False)
 
     def hyper_tn(self):
         import quimb.tensor as qtn
 
-        import tnmpa.solvers.tensor_factories as tfac
-
         tn = qtn.TensorNetwork([])
         data = np.ones((2, 2))
         data[1, 1] = 0.0
-        for c in self.factors:
+        for e in self.graph.edges:
             # add one tensor for each clause
-            tn_c = qtn.Tensor(
-                data=data, inds=(c.variables[0].name, c.variables[1].name)
-            )
+            tn_c = qtn.Tensor(data=data, inds=e)
             tn.add(tn_c, virtual=True)
+        for v in self.graph.nodes:
+            if hasattr(self.mu, "__len__"):
+                data = np.exp([1, self.mu[v]])
+            else:
+                data = np.exp([1, self.mu])
+            tn_c = qtn.Tensor(data=data, inds=(v,))
+            tn.add(tn_c, virtual=True)
+
         for v in tn.outer_inds():
             tn.add(
                 qtn.tensor_core.COPY_tensor(
